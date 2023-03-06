686398e3eea767f10e8025904733db806603b70a
YaMiN <yamin8000@yahoo.com>
2023-03-07 01:14:30 +0330

    reformat python codes

--- python/nMax_588.py
+++ python/nMax_588.py
@@ -1,8 +1,8 @@
 n = input()
 numbers = input()
 numbers_array = numbers.split(' ')
-max = int(numbers_array[0])
+maximum = int(numbers_array[0])
 for num in numbers_array:
-    if int(num) > max:
-        max = int(num)
-print(max)
+    if int(num) > maximum:
+        maximum = int(num)
+print(maximum)

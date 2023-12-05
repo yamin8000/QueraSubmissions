1716a3a22e8631f0753242d0e6e84afa1f6814e0
YaMiN <yamin8000@yahoo.com>
2023-12-05 15:16:53 +0330

    49535

--- rust/main_cargo/src/main.rs
+++ rust/main_cargo/src/main.rs
@@ -1,9 +1,11 @@
 mod question_3408;
 mod camera_2794;
 mod walnut_3540;
+mod report_49535;
 
 fn main() {
     //question_3408::question_3408();
     //camera_2794::camera_2794()
-    walnut_3540::main();
+    //walnut_3540::main();
+    report_49535::main();
 }
---
+++ rust/main_cargo/src/report_49535.rs
@@ -0,0 +1,28 @@
+pub fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let nums: Vec<i32> = input.split(' ')
+        .map(|s| s.trim().parse().unwrap())
+        .collect();
+    let (n, mut k) = (*nums.first().unwrap(), *nums.last().unwrap());
+
+    let mut bottles: Vec<i32> = Vec::new();
+    for _i in 0..n {
+        bottles.push(get_num())
+    }
+    for bottle in bottles {
+        if k <= 0 {
+            break;
+        }
+        k -= bottle
+    }
+    if k <= 0 {
+        println!("YES")
+    } else { println!("NO") }
+}
+
+fn get_num() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    return input.trim().parse().unwrap();
+}
